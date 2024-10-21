8226d185f675010291426997a692c04caf7c8652
missesenam <moneyesenam@gmail.com>
2024-10-21 14:12:03 +0100

    packages, services and contact pages done

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ function hidesidebar(){
     menuBar.classList.add('hidden')
     hamburgerbar.classList.remove('hidden')
 } 
+// more text
+function toggleMore() {
+    var moreText = document.getElementById("moreText");
+    var btn = document.querySelector(".btn");
+
+    if (moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btn.innerHTML = "Show Less";
+    } else {
+        moreText.style.display = "none";
+        btn.innerHTML = "Click More";
+    }
+}
